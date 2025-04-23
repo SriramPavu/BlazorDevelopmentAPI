5add8f65ddb5cf6a291dd983b760c523c7b93a70
SriramPavu <srirampavu11@gmail.com>
2025-04-23 23:46:34 +0200

    Add files via upload

---
+++ JobListingsMobile.WASM/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace JobListingsMobile.WASM
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
